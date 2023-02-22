df551ed136b6994df3b646b0595ed92af71f01d0
Greyn1 <dheryabengani@outlook.com>
2023-02-23 00:28:40 +0530

    added Side Drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,10 +1,17 @@
 import { Link, Outlet } from 'react-router-dom';
+import SideDrawer from './SideDrawer';
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = () => {
     return (
         <>
+            <SideDrawer>
+                <nav className='main-navigation__drawer-nav' >
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
             <MainHeader>
                 <button className='main-navigation__menu-btn' >
                     <span />
@@ -14,8 +21,8 @@ const MainNavigation = () => {
                 <h1 className='main-navigation__title'>
                     <Link to="/">Your Places</Link>
                 </h1>
-                <nav>
-                    ...
+                <nav className='main-navigation__header-nav' >
+                    <NavLinks />
                 </nav>
             </MainHeader>
             <main><Outlet /></main>
