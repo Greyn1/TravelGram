436e9495aa920d696adf8ca4fd00570a8ca2ac6a
Greyn1 <dheryabengani@outlook.com>
2023-02-24 23:27:03 +0530

    added portal for sideDrawer and backdrop, also added animation to sideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+
+import Backdrop from '../UIElements/Backdrop';
 import SideDrawer from './SideDrawer';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+
 import './MainNavigation.css';
 
 const MainNavigation = () => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
         <>
-            <SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler} >
                 <nav className='main-navigation__drawer-nav' >
                     <NavLinks />
                 </nav>
             </SideDrawer>
             <MainHeader>
-                <button className='main-navigation__menu-btn' >
+                <button className='main-navigation__menu-btn' onClick={openDrawerHandler} >
                     <span />
                     <span />
                     <span />
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,8 +1,16 @@
+import { createPortal } from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 import './SideDrawer.css';
 
 const SideDrawer = (props) => {
+  const portalContent = (
+    <CSSTransition in={props.show} timeout={200} classNames="slide-in-left" mountOnEnter unmountOnExit >
+      <aside className='side-drawer' onClick={props.onClick} >{props.children}</aside>
+    </CSSTransition>
+  );
+
   return (
-    <aside className='side-drawer' >{props.children}</aside>
+    createPortal(portalContent, document.getElementById('drawer-hook'))
   );
 }
 
