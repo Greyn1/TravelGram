56749ab05692f6eca29991f6e60bfa01630ca133
Greyn1 <dheryabengani@outlook.com>
2023-02-26 19:35:19 +0530

    added filter condition to show places with a particular userId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,36 +1,40 @@
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1200px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg',
+        title: 'Taj Mahal',
+        address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001',
+        description: 'One of the 7 wonders of the world',
+        creatorId: 'u1',
+        location: {
+            lat: 27.1751448,
+            lng: 77.9020665
+        }
+    },
+    {
+        id: 'p2',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1200px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg',
+        title: 'Taj Mahal',
+        address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001',
+        description: 'One of the 7 wonders of the world',
+        creatorId: 'u2',
+        location: {
+            lat: 27.1751448,
+            lng: 77.9020665
+        }
+    }
+]
 
 const UserPlaces = () => {
 
-    const DUMMY_PLACES = [
-        {
-            id: 'p1',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1200px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg',
-            title: 'Taj Mahal',
-            address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001',
-            description: 'One of the 7 wonders of the world',
-            creatorId: 'u1',
-            location: {
-                lat: 27.1751448,
-                lng: 77.9020665
-            }
-        },
-        {
-            id: 'p2',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1200px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg',
-            title: 'Taj Mahal',
-            address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001',
-            description: 'One of the 7 wonders of the world',
-            creatorId: 'u2',
-            location: {
-                lat: 27.1751448,
-                lng: 77.9020665
-            }
-        }
-    ]
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creatorId === userId);
 
   return (
-    <PlaceList items={DUMMY_PLACES} />
+    <PlaceList items={loadedPlaces} />
   );
 }
 
