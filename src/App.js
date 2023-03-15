6d41382af3c2101c384f87b7d6f1d9d7ac06365e
Greyn1 <dheryabengani@outlook.com>
2023-03-16 00:28:26 +0530

    added Auth Comp.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/:userId/places" element={<UserPlaces />} />
         <Route path="/places/new" element={<NewPlace />} />
         <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="/auth" element={<Auth/>} />
         <Route path="/*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
