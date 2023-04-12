cf9426f0143863d4a73a24375bdc3072b20598eb
Greyn1 <dheryabengani@outlook.com>
2023-04-12 19:46:38 +0530

    added patch and delete middleware for places routes

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,7 +1,7 @@
 import HttpError from '../models/http-error.js';
 import { v4 as uuidv4 } from 'uuid';
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
         id: 'p1',
         title: 'Taj Mahal',
@@ -30,18 +30,18 @@ export const getPlaceById = (req, res, next) => {
     res.json({place});
 }
 
-export const getPlaceByUserId = (req, res, next) => {
+export const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find((p) => {
+    const places = DUMMY_PLACES.filter((p) => {
         return p.creatorId === userId;
     });
 
-    if(!place){
+    if(!places || places.length === 0){
         return next(
-            new HttpError('Could not find a place for the provided user id.', 404)
+            new HttpError('Could not find places for the provided user id.', 404)
         );
     }
-    res.json({place});
+    res.json({places});
 }
 
 export const createPlace = (req, res, next) => {
@@ -57,6 +57,25 @@ export const createPlace = (req, res, next) => {
     DUMMY_PLACES.push(createdPlace);
     res.status(201).json({place: createdPlace}); 
     // status of 201 is used as a convention when something new is successfully created.
+}
+
+export const updatePlace = (req, res, next) => {
+    const {title, description} = req.body;
+    const placeId = req.params.pid;
+
+    const updatedPlace = {...DUMMY_PLACES.find(p => p.id === placeId)};
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+    updatedPlace.title = title;
+    updatedPlace.description = description;
+
+    DUMMY_PLACES[placeIndex] = updatedPlace;
+    res.status(200).json({place: updatedPlace});
+}
+
+export const deletePlace = (req, res, next) => {
+    const placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+    res.status(200).json({message: "Deleted the place."});
 }
 
 
